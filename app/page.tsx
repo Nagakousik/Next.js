c23f43d563fba4b21279375376fe5f9024fb8ff4
NagaKousik <rajanikumarkunapa123@gmail.com>
2026-04-15 20:05:09 +0530

    This is the favourite  movie website of me

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,10 @@
+import MovieCard from "./Components/MovieCard";
 
 
-export default function Page(){
-  return(
+export default function Page() {
+  return (
     <>
-    <h1 id="head">This is koushik</h1>
-
-    <div id="Theme">
-      <p id="con">This is kousik and this is my first start to the next.js</p>
-    </div>
-    <br></br>
-    <div id="content">
-      <h3 id="listhead">These are Favourite places :</h3>
-      <ol id="list">
-        <li>Nellore</li>
-        <br></br>
-        <li>Banglore</li>
-        <br></br>
-        <li>Munnar</li>
-        <br></br>
-        <li>Ladakh</li>
-      </ol>
-    </div>
+   <MovieCard/>
     </>
-  )
+  );
 }
